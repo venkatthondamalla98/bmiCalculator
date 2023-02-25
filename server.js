5e1a847cc23b7f570fb42ccf01be60ea50689cf9
venkatthondamalla <venkat.thondamalla@byjus.com>
2023-02-25 12:46:05 +0530

    feat: :sparkles: bmiCalculator
    bmiCalculator

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + '/calculator.html')
@@ -16,6 +15,19 @@ app.post("/", function(req, res){
     const result = num1 + num2
 
     res.send("Result is " + result)
+})
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html')
+})
+
+app.post("/bmicalculator", function(req, res){
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+
+    const bmi = weight / (height * height)
+
+    res.send("Your BMI is " + bmi)
 })
 
 app.listen(3000, function(){
